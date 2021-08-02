c275c0fd78cc0f22c0c8521436b5d4538677c465
Serjeri <hohachka@mail.ru>
2021-08-02 23:35:08 +0300

    I wrote Get query, wrote json, wrote PUT,DELETE, queries.

--- Client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket()
-sock.connect(('localhost', 9090))
-print("Сервер подключен")
-
-data = sock.recv(1024)
-sock.close()
-print = data
--- ServerRestApi.py
+++ ServerRestApi.py
@@ -1,23 +1,44 @@
 # написать rest api сервес по бронированию комнат
 # Делается
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-tasks = [
+
+rooms = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
+        'room': 'atelier',
+        'quantity': 10
+    },
+    {
+        'id': 2,
+        'room': 'one-room_apartment',
+        'quantity': 3
+    },
+    {
+        'id': 3,
+        'room': 'two-room_apartment',
+        'quantity': 5
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/rooms', methods=['GET'])
+def get_list():
+    return jsonify(rooms)
 
+@app.route('/rooms/<int:room_name>', methods=['PUT'])
+def update_room(room_name):
+    item = next((x for x in rooms if x ['room'] == room_name), None)
+    params = request.json
+    item.update(params)
+    return item
+
+@app.route('/rooms/<int:room_name>', methods=['DELETE'])
+def delete_room(room_name):
+    id_room, _ = next((x for x in enumerate(rooms) if x[1] ['room'] == room_name), (None,None))
+    rooms.pop(id_room)
+    return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
